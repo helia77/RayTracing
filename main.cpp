6c5020d74a0337d772c3c0d31dfcab46834aadc9
Helia Gohar <90401379+helia77@users.noreply.github.com>
2022-09-20 23:21:20 -0500

    added

--- main.cpp
+++ main.cpp
@@ -4,8 +4,26 @@
 #include<vector>
 #include<cmath>
 
-struct sphere;
-struct intersection;
+using namespace std;
+
+struct sphere {
+	// c, r, color
+	float radius;
+	float points[3];
+	float color[3];
+
+};
+
+struct intersection {
+	std::vector<float> point;
+	std::vector<float> color;
+
+	intersection(std::vector<float> pos, std::vector<float> col) {
+		point = pos;
+		color = col;
+	}
+};
+vector<sphere> ReadSphere(string s);
 
 void write_image(std::string filename, char* bytes, short N) {
 	std::ofstream outfile;
@@ -26,7 +44,35 @@ void write_image(std::string filename, char* bytes, short N) {
 	outfile.close();				// close the file
 }
 
-int main(int argc, char** argv)
+int main(int argc, char** argv[])
 {
-	
+	char* image = (char*)malloc(100 * 100 * 3);
+	for (int i = 0; i < 100; i++) {
+		for (int j = 0; j < 100; j++) {
+			image[j * 100*3 + i * 3 + 0] = '0';
+			image[j * 100*3 + i * 3 + 1] = 'g';
+			image[j * 100*3 + i * 3 + 2] = 'r';
+		}
+	}
+	write_image("spheres.tga", image, 100);
+
+	vector<sphere> spheres;
+	spheres = ReadSphere("spheres.txt");
+
+
+	return 0;
+}
+
+vector<sphere> ReadSphere(string s) {
+	ifstream rfile(s);
+	vector<sphere> spheres;
+
+	while (rfile) {
+		sphere s;
+		rfile >> s.points[0] >> s.points[1] >> s.points[2];
+		rfile >> s.radius;
+		rfile >> s.color[0] >> s.color[1] >> s.color[2];
+		spheres.push_back(s);
+	}
+	return spheres;
 }
